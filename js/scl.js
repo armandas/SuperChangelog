e7039814743fa65bbc266e1f854f2245459e200c
Armandas Jarusauskas <jarusauskas@gmail.com>
2013-08-23 20:55:26 +0100

    Adding files before it's too late.

---
+++ js/scl.js
@@ -0,0 +1,11 @@
+function setType()
+{
+	/*$('#is_bug_fix').toggleClass('tb-bug');
+
+	if ($('#is_bug_fix').hasClass('tb-bug')) {
+		$('#is_bug_fix span').text('Bug fix');
+	}
+	else {
+		$('#is_bug_fix span').text('Feature');
+	}*/
+}
