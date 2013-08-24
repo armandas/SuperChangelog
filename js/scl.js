83fb40285c5162b15b174ead9e9ca59cf1f63323
Armandas Jarusauskas <jarusauskas@gmail.com>
2013-08-24 12:20:03 +0100

    Some adjustments to CSS. Removing colour from message type checkbox.

--- js/scl.js
+++ js/scl.js
@@ -1,7 +1,7 @@
 function message_setTypeText()
 {
 	if ($('#is_bug_fix input').is(':checked'))
-		$('#is_bug_fix span').text('Bug fix');
+		$('#is_bug_fix span').text('Bug Fix');
 	else
 		$('#is_bug_fix span').text('Feature');
 }
