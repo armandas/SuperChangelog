76db88638d45aa21f3c713a02d24fd5fed0b8a6e
Armandas Jarusauskas <jarusauskas@gmail.com>
2013-08-26 20:45:28 +0100

    Whitespace changes only.

--- scl.php
+++ scl.php
@@ -5,15 +5,16 @@ error_reporting(E_ALL);
 require_once('include/db.php');
 
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_database);
+
 if ($mysql->connect_errno) {
-	echo "Failed to connect to MySQL: " . $mysql->connect_error;
+	print("Failed to connect to MySQL: " . $mysql->connect_error);
 }
 
 $product_list_query = "SELECT p.name, p.id, r.version
-					   FROM products AS p
-					   		LEFT JOIN releases as r ON p.id = r.product_id
-					   		LEFT JOIN releases AS r2 ON (r.product_id = r2.product_id AND r2.date > r.date)
-					   WHERE r2.date IS NULL";
+		       FROM products AS p
+		       LEFT JOIN releases as r ON p.id = r.product_id
+		       LEFT JOIN releases AS r2 ON (r.product_id = r2.product_id AND r2.date > r.date)
+		       WHERE r2.date IS NULL";
 
 if (isset($_GET['product_list'])) {
 	$products = array();
