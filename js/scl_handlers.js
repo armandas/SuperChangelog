dd7e49dc2a4d6d8dbf5f76cdb31d02cb0ada23ba
Armandas Jarusauskas <jarusauskas@gmail.com>
2013-08-28 19:11:49 +0100

    Moving javascript handlers into jquery document ready function.

--- js/scl_handlers.js
+++ js/scl_handlers.js
@@ -1,48 +1,51 @@
-$('#menu a').on('click', function()
+$(document).ready(function()
 {
-	displayPage(this.hash);
-});
+	$('#menu a').on('click', function()
+	{
+		displayPage(this.hash);
+	});
 
-$('input[name=is_bug_fix]').on('change', function()
-{
-	if ($(this).is(':checked'))
-		$(this).siblings('span').text('Bug Fix');
-	else
-		$(this).siblings('span').text('Feature');
-});
+	$('input[name=is_bug_fix]').on('change', function()
+	{
+		if ($(this).is(':checked'))
+			$(this).siblings('span').text('Bug Fix');
+		else
+			$(this).siblings('span').text('Feature');
+	});
 
-$('form:has(.products)').on('submit', function()
-{
-	if ($(this).find('input:checked').length == 0) {
-		$(this).find('.products').addClass('error');
-		return false;
-	}
-});
+	$('form:has(.products)').on('submit', function()
+	{
+		if ($(this).find('input:checked').length == 0) {
+			$(this).find('.products').addClass('error');
+			return false;
+		}
+	});
 
-$('.products').on('change', 'input', function()
-{
-	$(this).parents('.products').removeClass('error');
-});
+	$('.products').on('change', 'input', function()
+	{
+		$(this).parents('.products').removeClass('error');
+	});
 
-$('.products').on('change', 'input[name=download]', function()
-{
-	$('#log-text').load(SCL_GET_CHANGELOG_URL + '?view=' + $(this).val());
-});
+	$('.products').on('change', 'input[name=download]', function()
+	{
+		$('#log-text').load(SCL_GET_CHANGELOG_URL + '?view=' + $(this).val());
+	});
 
-$('.products').on('change', 'input[name=release]', function()
-{
-	var phText = 'Enter a new version number.';
+	$('.products').on('change', 'input[name=release]', function()
+	{
+		var phText = 'Enter a new version number.';
 
-	for (var i in scl_products) {
-		if (scl_products[i].id == $(this).val())
-			phText += ' Current release is ' + scl_products[i].release;
-	}
+		for (var i in scl_products) {
+			if (scl_products[i].id == $(this).val())
+				phText += ' Current release is ' + scl_products[i].release;
+		}
 
-	$('#release-text').attr('placeholder', phText);
-});
+		$('#release-text').attr('placeholder', phText);
+	});
 
-$('#footer').on('dblclick', function()
-{
-	displayPage('#admin');
-	document.getSelection().removeAllRanges();
+	$('#footer').on('dblclick', function()
+	{
+		displayPage('#admin');
+		document.getSelection().removeAllRanges();
+	});
 });
