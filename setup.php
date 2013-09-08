d1c48f77b7ae5a9260f9c5e80125b30c0b508108
Armandas Jarusauskas <jarusauskas@gmail.com>
2013-09-08 12:28:20 +0100

    Adding PHP compatibility test to the setup.

--- setup.php
+++ setup.php
@@ -8,6 +8,30 @@ define("SCL_SETUP_VERSION",	'1');
 define("SCL_SETUP_SIG",		'tmp/SCL_INSTALLED');
 define("SCL_SETUP_HASH", 	md5($db_host . $db_user . $db_database . SCL_SETUP_VERSION));
 
+$functions = array(
+	"json_encode",
+	"array_key_exists",
+	"file_put_contents",
+	"file_get_contents",
+	"property_exists",
+);
+
+$classes = array(
+	"mysqli" => array(
+		"query",
+		"prepare",
+		"close"
+	),
+	"mysqli_stmt" => array(
+		"bind_param",
+		"execute",
+		"bind_result",
+		"fetch"
+	),
+	"mysqli_result" => array(
+		"fetch_assoc"
+	)
+);
 
 $tables = array(
 	'scl_changelog' =>	"CREATE TABLE IF NOT EXISTS `scl_changelog` (
@@ -83,7 +107,7 @@ define("OUTPUT_SUCCESS",	'<div class="page">
 define("OUTPUT_FAIL",		'<div class="page">
 					<h2>Setup failed!</h2>
 					<div class="products">SuperChangelog was not set up correctly.
-					Fix the errors above and <a href="setup.php">try again</a>.</div>
+					Please fix the errors above and <a href="setup.php">try again</a>.</div>
 				 </div>');
 
 define("OUTPUT_STEP", 		'<div class="page">
@@ -92,9 +116,17 @@ define("OUTPUT_STEP", 		'<div class="page">
 				 </div>');
 
 
+define("MESSAGE_OK", 		'OK.');
 define("MESSAGE_DB_CONN", 	'Database connection failed. Check your settings in <code>include/db.php</code>.');
 define("MESSAGE_DB_TABLES", 	'Error: could not create the required tables. Contact your system administrator.');
-define("MESSAGE_TMP_DIR",	'Error: directory <code>tmp/</code> is not writable.');
+define("MESSAGE_TMP_DIR",	'Error: cannot write to <code>tmp/</code> directory. Check that the folder exits
+				 and has correct permissions.');
+define('MESSAGE_PHP',		'%s
+				 <a target="_blank" href="http://php.net/manual-lookup.php?pattern=%2$s">%2$s</a>
+				 is not available.<br>');
+define('MESSAGE_COMPAT',	'<br>Due to compatibility errors, SCL setup cannot be continued.
+				 Please fix the errors above and <a href="setup.php">try again</a>.');
+
 
 
 
@@ -102,7 +134,44 @@ $err = false;
 $output = OUTPUT_HEADER;
 
 /*****************************************************************************/
+/* Check PHP compatibility. If this step fails, the setup cannot continue.   */
+/*****************************************************************************/
+$title = "PHP compatibility";
+$message = '';
+foreach ($functions as $function) {
+	if (!function_exists($function)) {
+		$message .= sprintf(MESSAGE_PHP, 'Function', $function);
+		$err = true;
+	}
+}
 
+foreach ($classes as $class => $methods) {
+	if (!class_exists($class)) {
+		$message .= sprintf(MESSAGE_PHP, 'Class', $class);
+		$err = true;
+		continue;
+	}
+
+	foreach ($methods as $method) {
+		if (!method_exists($class, $method)) {
+			$message .= sprintf(MESSAGE_PHP, 'Method', $class . '::' . $method);
+			$err = true;
+		}
+	}
+}
+
+if($err) {
+	$message .= MESSAGE_COMPAT;
+
+	$output .= sprintf(OUTPUT_STEP, $title, $message);
+	$output .= OUTPUT_FOOTER;
+	print($output);
+	exit();
+}
+
+/*****************************************************************************/
+/* Check for setup signature and quit if the system is already set up.       */
+/*****************************************************************************/
 $setup_hash = file_get_contents(SCL_SETUP_SIG);
 if (SCL_SETUP_HASH === $setup_hash) {
 	$output .= OUTPUT_SUCCESS . OUTPUT_FOOTER;
@@ -111,7 +180,8 @@ if (SCL_SETUP_HASH === $setup_hash) {
 }
 
 /*****************************************************************************/
-
+/* Check database settings.                                                  */
+/*****************************************************************************/
 $title = "Database connection";
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_database);
 if ($mysql->connect_errno) {
@@ -120,12 +190,13 @@ if ($mysql->connect_errno) {
 	$err = true;
 }
 else {
-	$message = 'OK.';
+	$message = MESSAGE_OK;
 }
 $output .= sprintf(OUTPUT_STEP, $title, $message);
 
 /*****************************************************************************/
-
+/* Check database tables and create them if necessary.                       */
+/*****************************************************************************/
 $err2 = false;
 $title = "Database structure";
 foreach ($tables as $table => $query) {
@@ -142,24 +213,26 @@ if ($err2) {
 	$message = MESSAGE_DB_TABLES;
 }
 else {
-	$message = 'OK.';
+	$message = MESSAGE_OK;
 }
 $output .= sprintf(OUTPUT_STEP, $title, $message);
 
 /*****************************************************************************/
-
+/* Check that the temporary folder exists and is writable.                   */
+/*****************************************************************************/
 $title = "Temporary folder";
 if (!is_writable("tmp/")) {
 	$message = MESSAGE_TMP_DIR;
 	$err = true;
 }
 else {
-	$message = 'OK.';
+	$message = MESSAGE_OK;
 }
 $output .= sprintf(OUTPUT_STEP, $title, $message);
 
 /*****************************************************************************/
-
+/* Print the result and create the setup signature if successful.            */
+/*****************************************************************************/
 if ($err) {
 	$output .= OUTPUT_FAIL;
 }
@@ -167,8 +240,6 @@ else {
 	file_put_contents(SCL_SETUP_SIG, SCL_SETUP_HASH);
 	$output .= OUTPUT_SUCCESS;
 }
-
-/*****************************************************************************/
 
 $output .= OUTPUT_FOOTER;
 print($output);
